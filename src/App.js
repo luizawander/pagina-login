a2448621d29fde80a5d2adbd893c50758a4d057c
luizawander <luizasalesgithub@gmail.com>
2024-03-29 15:12:00 -0300

    base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div  className="container"> 
+      <div className="conteiner-login">
+        <div className="wrap-login">
+          <form className="login-form">
+              <span className="login-form-tittle"> Bem Vindo!</span>
+              <span className="login-form-tittle"> 
+              </span>
+          </form>
+        </div> {/* div wrap-login */ }
+      </div> {/* div conteiner-login */ }
+    </div> 
   );
 }
 
