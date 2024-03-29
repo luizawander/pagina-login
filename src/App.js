fd0af216fc787f7f6620f5057eef84d0e9c3bb85
luizawander <luizasalesgithub@gmail.com>
2024-03-29 15:41:40 -0300

    input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import logoIMG from './assets/logo.jpeg'
+
 function App() {
   return (
     <div  className="container"> 
       <div className="conteiner-login">
         <div className="wrap-login">
           <form className="login-form">
+
               <span className="login-form-tittle"> Bem Vindo!</span>
-              <span className="login-form-tittle"> 
+
+              <span className="login-form-title">
+                <img src={logoIMG} alt="Logo com Raio" />
               </span>
+
+              <div className='wrap-input'>
+                  <input type='email' />
+                  <span className='focus-input' data-placeholder="Email"> </span>
+              </div>
+
+              <div className='wrap-input'>
+                  <input type='Password' />
+                  <span className='focus-input' data-placeholder="Password"> </span>  
+              </div>
+
           </form>
         </div> {/* div wrap-login */ }
       </div> {/* div conteiner-login */ }
