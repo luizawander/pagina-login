48f40cb33ef2f0d4c170c63acea0d82926d0053c
luizawander <luizasalesgithub@gmail.com>
2024-03-30 19:22:07 -0300

    radial-gradient

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ function App() {
                   <span className='focus-input' data-placeholder="Password"> </span>  
               </div>
 
-              <div className='Container-login-form-btn'>
+              <div className='container-login-form-btn'>
                 <button className='login-form-btn'>Login</button>
               </div>
 
               <div className='text-center'>
                 <span className='txt1'> Não possui conta?</span>
-                <a className="txt2" href="#">Criar conta.</a>
+                <a className="txt2" href="#"> Criar.</a>
             
               </div>
 
