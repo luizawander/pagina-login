b5f0687c18479cfef68aa34e699ea619e2a137ea
luizawander <luizasalesgithub@gmail.com>
2024-03-31 10:31:22 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, { useState } from 'react';
 import logoIMG from './assets/logozap.png'
 import './styles.css'
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
   return (
     <div  className="container"> 
       <div className="conteiner-login">
@@ -15,12 +19,12 @@ function App() {
           </span>
 
               <div className='wrap-input'>
-                  <input className='input' type='email' />
+                  <input className={email !== "" ? "has-val input" : "input"} type='email' value={email} onChange={e => setEmail(e.target.value)} />  
                   <span className='focus-input' data-placeholder="Email"> </span>
               </div>
 
               <div className='wrap-input'>
-                  <input className='input' type='Password' />
+                  <input className={password !== "" ? "has-val input" : "input"} type='Password' value={password} onChange={e => setPassword(e.target.value)} />
                   <span className='focus-input' data-placeholder="Password"> </span>  
               </div>
 
